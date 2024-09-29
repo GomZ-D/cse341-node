aff35140a5e4a51c70fb492e0b3f72b9ce27de93
GomZ-D <gomezdiego09@gmail.com>
2024-09-29 02:10:27 -0400

    Methods put, delete and post

--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,5 @@
 const mongodb = require('../data/database');
+// const { unsubscribe } = require('../routes');
 const ObjectId = require('mongodb').ObjectId;
 
 
@@ -19,7 +20,7 @@ const getAll = async (req, res, next) => {
   };
   
   //------------------------------------
-  //--FUNCTION TO GET A SINGLE CONTAT
+  //--FUNCTION TO GET A SINGLE USER
   //------------------------------------
   
   const getSingle = async (req, res, next) => {
@@ -39,8 +40,88 @@ const getAll = async (req, res, next) => {
   }
   };
 
+//------------------------------------
+//--FUNCTION TO CREATE A USER
+//------------------------------------
+
+ const createUser = async(req,res) => {
+  const user = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    email: req.body.email,
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday 
+  };
+
+    const result = await mongodb
+      .getDatabase()
+      .db()
+      .collection('users')
+      .insertOne(user);
+      
+  if (result.acknowledged) {
+    res.status(204).send();
+  }else {
+    res.status(500).json(result.error || 'Error while Creating User');
+  }
+}
+
+//------------------------------------
+//--FUNCTION TO UPDATE A USER
+//------------------------------------
+
+const updateUser = async (req, res) => {
+ const userId = new ObjectId(req.params.id);
+
+ const user = {
+  firstName: req.body.firstName,
+  lastName: req.body.lastName,
+  email: req.body.email,
+  favoriteColor: req.body.favoriteColor,
+  birthday: req.body.birthday 
+ };
+
+ const result = await mongodb
+  .getDatabase()
+  .db()
+  .collection('users')
+  .replaceOne({_id: userId}, user); 
+ 
+ if(result.modifiedCount > 0) {
+  res.status(204).send();
+ } else {
+  res.status(500).json(result.error || "Error while Updating User")
+ }
+}
+
+//------------------------------------
+//--FUNCTION TO DELETE A USER
+//------------------------------------
+
+const deleteUser = async (req, res) => {
+  const userId = new ObjectId(req.params.id);
+
+  const result = mongodb
+  .getDatabase()
+  .db()
+  .collection('users')
+  .deleteOne({_id:userId});
+
+  // console.log(result);
+
+//  if(result.deletedCount > 0) {
+  if(result.deletedCount > 0){
+  res.status(204).send();
+  }else {
+  res.status(500).json(result.error || "Error While Deleting User/s")
+ }
+}
 
   module.exports = {
     getAll,
-    getSingle
+    getSingle,
+    deleteUser,
+    createUser,
+    updateUser
   }
+
--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
- 
 const usersController = require('../controllers/users');
 
-
+//----------------------------------------------
 //--ROUTES
+//----------------------------------------------
+
+
 //Get all the collection.
 router.get('/', usersController.getAll);
 //Get ony document.
 router.get('/:id', usersController.getSingle);
+//create
+router.post('/', usersController.createUser);
+//DELETE USER
+router.delete('/:id', usersController.deleteUser);
+//UPDATE USER
+router.put('/:id', usersController.updateUser);
 
 module.exports = router;
